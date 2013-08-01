21f5e893726d6c709a93623d671a26fae111d757
kamrad117 <kamrad117@gmail.com>
2013-08-01 19:12:28 +0300

    remove HTTPRequest

--- lib/targetprocess/entities_initializer.rb
+++ lib/targetprocess/entities_initializer.rb
@@ -1,7 +1,14 @@
 module Targetprocess
-  ENTITIES = YAML.load_file(EntityCommons::ATTR_FILE).keys.collect! do |key|
-    key.titleize 
-  end
+  ENTITIES = ["Task", "Userstory", "Feature", "Bug", "User", "Project", 
+              "Release", "Iteration", "Request", "Testcase", "Impediment", 
+              "Comment", "Process", "Priority", "Severity", "Entitystate", 
+              "Program", "Testplan", "Testplanrun", "Testcaserun", "Time", 
+              "Assignment", "Role", "Roleeffort", "Projectmember", "Build", 
+              "Company", "Customactivity", "Attachment", "Entitytype", 
+              "General", "Assignable", "Generaluser", "Requesttype", "Message",
+               "Messageuid", "Milestone", "Relation", "Relationtype", 
+               "Requester", "Revision", "Revisionfile", "Tag", "Team", 
+               "Teamiteration", "Teammember", "Teamproject"]
 
   ENTITIES.each do |name|
     eval "class #{name}; include EntityCommons; end"
